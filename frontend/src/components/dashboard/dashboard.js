36dd1cdb7e57eccb015002de55c8e17ccffc45c0
Jayashree Sridhar <jayashree.sridhar@sjsu.edu>
2021-05-03 17:01:19 -0700

    Changes class to className

--- frontend/src/components/dashboard/dashboard.js
+++ frontend/src/components/dashboard/dashboard.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { getPosts } from '../../actions/postActions';
-import ago from 's-ago';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getPosts } from "../../actions/postActions";
+import ago from "s-ago";
+import { Link } from "react-router-dom";
 
 class Dashboard extends Component {
   componentDidMount() {
     const { isAuthenticated } = this.props.auth;
-    if(isAuthenticated){
+    if (isAuthenticated) {
       //this.props.getDashboardDetails(user.user_id);
       // TODO: filter posts related to user
       this.props.getPosts();
@@ -20,31 +20,42 @@ class Dashboard extends Component {
     const { postsDetails } = this.props.posts;
 
     return (
-      isAuthenticated && <div className="posts">
-      {postsDetails.map(post => {
-        return (
-        <div className="row mt-4">
-          <div className="col-1">
-            <div className="d-flex flex-column ps-5 mt-2">
-              <i data-test="fa" class="fa fa-lg fa-angle-up"></i>
-              <p className="fs-3 mt-2 pe-1">{post.score}</p>
-              <i data-test="fa" class="fa fa-lg fa-angle-down mt-n1"></i>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card">
-              <div className="card-body">
-              <p className="card-text"> /r/{post.communityName} &nbsp; Posted by u/{post.author} &nbsp; <span className="fw-lighter fst-italic text-muted">{ago(new Date(post.createdAt))}</span></p>
-                <h5 className="card-title">{post.title}</h5>
-                <p className="card-text">{post.text} </p>
-                <p className="card-text"><Link to={`/comments/${post._id}`}> Comments</Link></p>
+      isAuthenticated && (
+        <div className="posts">
+          {postsDetails.map((post) => {
+            return (
+              <div className="row mt-4" key={post._id}>
+                <div className="col-1">
+                  <div className="d-flex flex-column ps-5 mt-2">
+                    <i data-test="fa" class="fa fa-lg fa-angle-up"></i>
+                    <p className="fs-3 mt-2 pe-1">{post.score}</p>
+                    <i data-test="fa" class="fa fa-lg fa-angle-down mt-n1"></i>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="card">
+                    <div className="card-body">
+                      <p className="card-text">
+                        {" "}
+                        /r/{post.communityName} &nbsp; Posted by u/{post.author}{" "}
+                        &nbsp;{" "}
+                        <span className="fw-lighter fst-italic text-muted">
+                          {ago(new Date(post.createdAt))}
+                        </span>
+                      </p>
+                      <h5 className="card-title">{post.title}</h5>
+                      <p className="card-text">{post.text} </p>
+                      <p className="card-text">
+                        <Link to={`/comments/${post._id}`}> Comments</Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       )
-      })}
-    </div>
     );
   }
 }
@@ -55,7 +66,7 @@ Dashboard.propTypes = {
   posts: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   posts: state.posts,
   auth: state.auth,
 });
--- frontend/src/components/layout/navbar.js
+++ frontend/src/components/layout/navbar.js
@@ -19,22 +19,25 @@ class Navbar extends Component {
     // const isProfile = profile != null ? true : false;
     return (
       <div className="redditNavbar">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container-fluid">
-            <Link class="navbar-brand" to="/dashboard">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/dashboard">
               <img alt="logo" src={logo} height="25px" />
             </Link>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav w-100 d-flex justify-content-end">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav w-100 d-flex justify-content-end">
                 {!isAuthenticated && (
                   <>
-                    <li class="nav-item">
-                      <Link class="nav-link" to="/login">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/login">
                         <Button className="login">Log In</Button>
                       </Link>
                     </li>
-                    <li class="nav-item">
-                      <Link class="nav-link" to="/register">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/register">
                         <Button className="signup">Sign Up</Button>
                       </Link>
                     </li>
@@ -45,17 +48,17 @@ class Navbar extends Component {
                     <li className="tabs nav-item">
                       <Link to="/communities">Communities</Link>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                       <Link to="/message">
                         <IconButton>
                           <ChatIcon color="#0854a5" />
                         </IconButton>
                       </Link>
                     </li>
-                    <li class="nav-item">
-                      <div class="dropdown">
+                    <li className="nav-item">
+                      <div className="dropdown">
                         <button
-                          class="userName btn btn-secondary dropdown-toggle"
+                          className="userName btn btn-secondary dropdown-toggle"
                           type="button"
                           id="dropdownMenuButton1"
                           data-bs-toggle="dropdown"
@@ -64,16 +67,16 @@ class Navbar extends Component {
                           {user.name}
                         </button>
                         <ul
-                          class="dropdown-menu"
+                          className="dropdown-menu"
                           aria-labelledby="dropdownMenuButton1"
                         >
                           <li>
-                            <Link class="nav-link" to="/invites">
+                            <Link className="nav-link" to="/invites">
                               View Invites
                             </Link>
                           </li>
                           <li>
-                            <Link class="nav-link" to="/">
+                            <Link className="nav-link" to="/">
                               Logout
                             </Link>
                           </li>
