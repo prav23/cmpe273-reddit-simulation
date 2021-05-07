a8ad19bfb56bdc3c65fac7f8f133b3aadb74608d
Yihu <alismail@foxmail.com>
2021-05-06 17:47:29 -0700

    performance: getMessagesRedisKafka

--- kafka-backend/services/performance/index.js
+++ kafka-backend/services/performance/index.js
@@ -1,6 +1,7 @@
 "use strict";
 const { createUsers } = require("./createUsers");
 const { createMessages } = require("./createMessages");
+const { getMessages } = require("./getMessages");
 
 let handle_request = (msg, callback) => {
   switch (msg.route) {
@@ -9,6 +10,9 @@ let handle_request = (msg, callback) => {
       break;
     case "create_messages":
       createMessages(msg, callback);
+      break;
+    case "get_messages":
+      getMessages(msg, callback);
       break;
   }
 };
